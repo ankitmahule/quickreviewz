c083ad3473e295c702c8ac6452e52122db7732a7
Ankit Mahule <ank.mahule@gmail.com>
2022-11-26 01:22:33 +0530

    Changes for responsive design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': {'min': '320px', 'max':'639px'},
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
